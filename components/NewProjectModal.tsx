c11384b1d1fc6c8481d552e4bc2b35c3b5e38412
SantiagoVira <santiagovira06@gmail.com>
2025-01-14 00:51:08 -0500

    Finish new project modal

--- components/NewProjectModal.tsx
+++ components/NewProjectModal.tsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, TextInput } from "react-native";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { CustomModal } from "./CustomModal";
 import { ThemedText } from "./ThemedText";
+import { useState } from "react";
 
 export const NewProjectModal: React.FC<{
   isVisible: boolean;
   onClose: () => void;
 }> = ({ isVisible, onClose }) => {
+  const [name, setName] = useState("");
   return (
     <CustomModal isVisible={isVisible} onClose={onClose}>
       <ThemedText type="subtitle">New Project</ThemedText>
@@ -15,12 +24,45 @@ export const NewProjectModal: React.FC<{
 
       <TextInput
         style={styles.input}
-        placeholder="Ex. My Favorite Tree"></TextInput>
+        placeholder="Ex. My Favorite Tree"
+        value={name}
+        onChangeText={setName}></TextInput>
+      <View style={styles.submitContainer}>
+        <Pressable
+          style={[styles.button, name === "" && styles.disabled]}
+          disabled={name === ""}
+          onPressOut={() => {
+            alert(name);
+            setName("");
+            onClose();
+          }}>
+          <Text style={styles.buttonText}>Create</Text>
+        </Pressable>
+      </View>
     </CustomModal>
   );
 };
 
 const styles = StyleSheet.create({
+  disabled: {
+    opacity: 0.5,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+  },
+  button: {
+    backgroundColor: "#4B365E",
+    width: 76,
+    padding: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  submitContainer: {
+    marginTop: 16,
+    alignItems: "flex-end",
+  },
   inputLabel: {
     color: "white",
     marginTop: 18,
@@ -34,6 +76,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 12,
     width: "100%",
+    color: "rgba(255,255,255,0.8)",
     backgroundColor: "rgba(0,0,0,0.3)",
   },
 });
